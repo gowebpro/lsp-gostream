b9bd4955704264591f6306d392cd36e7cefa511c
Chiffa <i@ars-team.ru>
2016-09-24 20:55:04 +0500

    Avatars

--- config/config.php
+++ config/config.php
@@ -13,5 +13,6 @@
 
 $config = array();
 $config['block_count'] = 10;		// сколько записей выводить в блоке "Прямой эфир"
+$config['block_avatar'] = false;	// показывать в блоке аватары
 
 return $config;
